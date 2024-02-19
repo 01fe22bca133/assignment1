5aa2d9c4c3e8e9590fe2b89f3c2160e277f3ae48
01fe22bca133 <01fe22bca133@kletech.ac.in>
2024-02-19 13:47:13 +0530

    first commit

---
+++ Primenumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Primenumber {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter ur range");
+
+        int n = s.nextInt();
+        for (int i = 2; i < n; i++) {
+            int factors = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    factors++;
+                }
+            }
+            if (factors == 2) {
+                System.out.println(i);
+            }
+        }
+    }
+
+}
